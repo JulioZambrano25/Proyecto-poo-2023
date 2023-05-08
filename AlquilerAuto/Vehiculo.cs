a602437362b095073bac9577bf850245581af074
JulioZambrano <juliozambrano201186@gmail.com>
2023-05-07 21:05:29 -0500

    TERCER CAMBIO

--- AlquilerAuto/Vehiculo.cs
+++ AlquilerAuto/Vehiculo.cs
@@ -7,5 +7,38 @@ namespace AlquilerAuto
 {
     public class Vehiculo
     {
+        protected int id;
+        protected string modelo;
+        protected string marca;
+        protected string kilometraje;
+        protected string matricula;
+        public Vehiculo() { }
+        public int Id
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public string Modelo
+        {
+            set { modelo = value; }
+            get { return modelo; }
+        }
+        public string Marca
+        {
+            set {  marca = value;}
+            get { return marca; }
+        }
+        public string Kilometraje
+        {
+            set { kilometraje=value; }
+            get { return kilometraje; }
+        }
+        public string Matricula
+        {
+            set { matricula = value;}
+            get {return marca; }
+        }
+        
     }
+    
 }
